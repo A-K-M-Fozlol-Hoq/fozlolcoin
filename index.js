e9e1c431c9862a49e37a18a01dc18b7e2461fec6
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2021-11-28 21:27:09 +0600

    creating first block chain

---
+++ index.js
@@ -0,0 +1,28 @@
+const sha256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(timestamp, data, previousHash) {
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+  calculateHash() {
+    return sha256(
+      this.timestamp + JSON.stringify(this.data) + this.previousHash
+    ).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [];
+  }
+  addBlock(newBlock) {
+    this.chain.push(newBlock);
+  }
+}
+const fozlolcoin = new Blockchain();
+const block = new Block("2022-01-01", { amount: 5 }, "ABCD");
+fozlolcoin.addBlock(block);
+console.log(fozlolcoin);
