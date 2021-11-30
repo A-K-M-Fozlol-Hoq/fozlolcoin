74650ed95b897ba3022e9f449db5deaf46ace3b4
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2021-11-30 14:58:48 +0600

    added add block functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sha256 = require("crypto-js/sha256");
 class Block {
-  constructor(timestamp, data, previousHash) {
+  constructor(timestamp, data, previousHash="") {
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
@@ -15,14 +15,22 @@ class Block {
 
 class Blockchain {
   constructor() {
-    this.chain = [];
+    this.chain = [this.generateGenesisBlock()];
+  }
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+  generateGenesisBlock() {
+    return new Block("1900-01-01", "genesis", "0000");
   }
   addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
 }
 const fozlolcoin = new Blockchain();
-const block = new Block("2022-01-01", { amount: 5 }, "ABCD");
+const block = new Block("2022-01-01", { amount: 5 });
 fozlolcoin.addBlock(block);
 console.log(block);
 console.log(fozlolcoin);
