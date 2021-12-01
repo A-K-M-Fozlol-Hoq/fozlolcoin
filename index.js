8c27a846a99449e0d13ebf563d577d555fba6075
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2021-12-01 17:03:18 +0600

    added isBLockchainvalid function

--- index.js
+++ index.js
@@ -28,9 +28,26 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+  isBlockchainValid(){
+    for(let i=1; i<this.chain.length; i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i-1];
+      if(currentBlock.hash !== currentBlock.calculateHash()){
+        return false;
+      }
+      if(currentBlock.previousHash !== previousBlock.hash){
+        return false;
+      }
+    }
+    return true;
+  }
 }
 const fozlolcoin = new Blockchain();
 const block = new Block("2022-01-01", { amount: 5 });
 fozlolcoin.addBlock(block);
 console.log(block);
-console.log(fozlolcoin);
+console.log(fozlolcoin.isBlockchainValid());
+
+fozlolcoin.chain[1].data="hacked";
+console.log(fozlolcoin.isBlockchainValid());
+
